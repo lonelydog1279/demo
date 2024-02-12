e1f3ae64b73d94c48006acfa4e8724f322857558
lonelydog1279 <787652644@qq.com>
2024-02-12 19:57:10 +0800

    test github webhook push

--- src/main/java/com/example/demo/demos/web/BasicController.java
+++ src/main/java/com/example/demo/demos/web/BasicController.java
@@ -33,6 +33,7 @@ public class BasicController {
     @ResponseBody
     public String hello(@RequestParam(name = "name", defaultValue = "unknown user") String name) {
         return "Hello " + name;
+
     }
 
     // http://127.0.0.1:8080/user
